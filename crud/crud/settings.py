fee51176b1486a14d09a8cc7f8f49ffe6a191877
jackwise254 <jackwise179@gmail.com>
2022-07-24 15:14:51 +0300

    added edit folder

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -73,13 +73,16 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'djangodb',  
+        'USER':'root',  
+        'PASSWORD':'mysql',  
+        'HOST':'localhost',  
+        'PORT':'3306'  
+    }  
+}  
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
